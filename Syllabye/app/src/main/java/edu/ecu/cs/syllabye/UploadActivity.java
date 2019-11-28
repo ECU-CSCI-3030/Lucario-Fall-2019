29de210b7c8adef77b98ae25e10830f602f424ea
MTBao <michael.trinh55@yahoo.com>
2019-11-27 20:33:30 -0500

    UploadButton in UploadActivity allows user to choose a PDF file
    Author: Michael Trinh <trinhm14@students.ecu.edu>
    Date: Wed Nov 27 2019
    Issue: #9
    
    The upload button in the Syllabye application's UploadActivity.java allows the user to choose a file from the phone's internal storage. I have limited the file selections to .pdf as we are assuming that we have a scenario where we can expect high resolution files. After the user selects the pdf they want, the filepath is shown on the UploadActivity screen so the user knows their file has been selected.

--- Syllabye/app/src/main/java/edu/ecu/cs/syllabye/UploadActivity.java
+++ Syllabye/app/src/main/java/edu/ecu/cs/syllabye/UploadActivity.java
@@ -1,14 +1,81 @@
 package edu.ecu.cs.syllabye;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.util.regex.Pattern;
 
 public class UploadActivity extends AppCompatActivity {
+
+    Button mFileButton;
+    TextView mFileText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+        }
+        mFileButton = (Button) findViewById(R.id.fileButton);
+        mFileText = (TextView) findViewById(R.id.fileText);
+        mFileText.setTextColor(Color.RED);
+
+        mFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialFilePicker()
+                        .withActivity(UploadActivity.this)
+                        .withRequestCode(1000)
+                        .withHiddenFiles(true)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            // Do anything with file
+            mFileText.setText(filePath);
+            mFileText.setTextColor(Color.RED);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1001:{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this, "Permission not granted!",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
