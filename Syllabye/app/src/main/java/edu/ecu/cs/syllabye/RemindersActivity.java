1816b9e8fd0e49fc69d2c32c2e95459cee152d67
Deric Martinez <50567951+DericMartinez@users.noreply.github.com>
2019-12-08 23:57:23 -0500

    Back Button
    Added Back Button to reminders screen. This will be implemented to every screen.

--- Syllabye/app/src/main/java/edu/ecu/cs/syllabye/RemindersActivity.java
+++ Syllabye/app/src/main/java/edu/ecu/cs/syllabye/RemindersActivity.java
@@ -2,13 +2,29 @@ package edu.ecu.cs.syllabye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RemindersActivity extends AppCompatActivity {
+    Button mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+
+        mBackButton = (Button) findViewById(R.id.back);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Back Button
+                Intent intent = new Intent(RemindersActivity.this, SyllabyeActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
