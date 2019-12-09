944ada79ed296695dc1e7e6b705a599ac5b37eae
MTBao <michael.trinh55@yahoo.com>
2019-12-08 22:48:29 -0500

    Added onClickScanButton() functionality and onActivityResult() functionality
    Author: Michael Trinh <trinhm14@students.ecu.edu>
    Date: Sun Dec 8 2019
    Issue: #11 #12
    
    Added functionality to onClickScanButton() to check the user permissions and prepare to store data from camera. Added onActivityResult() to prep Tesseract on text recognition from pictures and to store the taken photo as a thumbnail bitmap.

--- Syllabye/app/src/main/java/edu/ecu/cs/syllabye/TesseractActivity.java
+++ Syllabye/app/src/main/java/edu/ecu/cs/syllabye/TesseractActivity.java
@@ -1,12 +1,35 @@
 package edu.ecu.cs.syllabye;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
 public class TesseractActivity extends AppCompatActivity {
@@ -14,6 +37,20 @@ public class TesseractActivity extends AppCompatActivity {
     private Context context;
     int PERMISSION_ALL = 1;
     boolean flagPermissions = false;
+    protected String mCurrentPhotoPath;
+    private ProgressDialog mProgressDialog;
+    private Uri photoURI1;
+    private Uri oldPhotoURI;
+
+    private static final String errorFileCreate = "Error file create!";
+    private static final String errorConvert = "Error convert!";
+    private static final int REQUEST_IMAGE1_CAPTURE = 1;
+
+    @BindView(R.id.ocr_image)
+    ImageView firstImage;
+
+    @BindView(R.id.ocr_text)
+    TextView ocrText;
 
     String [] PERMISSIONS = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -27,6 +64,90 @@ public class TesseractActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tesseract);
     }
 
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("MMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, // prefix
+                ".jpg", // suffix
+                storageDir // directory
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @OnClick (R.id.scan_button)
+    void onClickScanButton() {
+        // check permissions
+        if (!flagPermissions) {
+            checkPermissions();
+            return;
+        }
+        // prepare intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                Toast.makeText(context, errorFileCreate, Toast.LENGTH_SHORT).show();
+                Log.i("File error", e.toString());
+            }
+            // Continue only if File was successfully created
+            if (photoFile != null) {
+                oldPhotoURI = photoURI1;
+                photoURI1 = Uri.fromFile(photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI1);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE1_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_IMAGE1_CAPTURE: {
+                if (resultCode == RESULT_OK) {
+                    Bitmap bmp = null;
+                    try {
+                        InputStream is = context.getContentResolver().openInputStream(photoURI1);
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        bmp = BitmapFactory.decodeStream(is, null, options);
+
+                    } catch (Exception e) {
+                        Log.i(getClass().getSimpleName(), e.getMessage());
+                        Toast.makeText(context, errorConvert, Toast.LENGTH_SHORT).show();
+                    }
+
+                    firstImage.setImageBitmap(bmp);
+                    //doOCR(bmp);
+
+                    OutputStream os;
+                    try {
+                        os = new FileOutputStream(photoURI1.getPath());
+                        if (bmp != null) {
+                            bmp.compress(Bitmap.CompressFormat.JPEG, 100, os);
+                        }
+                        os.flush();
+                        os.close();
+                    } catch (Exception e) {
+                        Log.e(getClass().getSimpleName(), e.getMessage());
+                        Toast.makeText(context, errorFileCreate, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    photoURI1 = oldPhotoURI;
+                    firstImage.setImageURI(photoURI1);
+                }
+            }
+        }
+    }
     void checkPermissions() {
         if (!hasPermissions(context, PERMISSIONS)) {
             requestPermissions(PERMISSIONS, PERMISSION_ALL);
